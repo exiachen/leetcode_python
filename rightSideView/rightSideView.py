fdc63184b7a34101c06d8bf375f5d737ba851f29
Chen Chen <exiachenchen@gmail.com>
2015-05-03 18:11:11 +0800

    merge and modify from master

--- rightSideView/rightSideView.py
+++ rightSideView/rightSideView.py
@@ -43,30 +43,3 @@ class Solution:
         self.treePathToListRe(root.left, result, (path + [root.val]))
 
         return
-        
-
-if __name__ == "__main__":
-    root = TreeNode(1)
-
-    node2 = TreeNode(2)
-    node3 = TreeNode(3)
-
-    node4 = TreeNode(4)
-    node5 = TreeNode(5)
-
-    node2.right = node5
-    node3.right = node4
-
-    root.right = node3
-    root.left = node2
-
-    s = Solution()
-
-    print s.rightSideView(root)
-
-
-
-
-
-
-        
